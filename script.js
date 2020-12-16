67c922ced9b99281a85b3a960c4410620c5200d7
FernanEd <fernandoed.bm@gmail.com>
2020-12-15 22:18:08 -0600

    2.0 added css, not fully doe

---
+++ script.js
@@ -0,0 +1,17 @@
+
+
+let button = document.querySelector("#submit-book");
+
+
+button.addEventListener("click", () => {
+    closeForm();
+})
+
+
+
+function closeForm(){
+    //Fisically close it
+
+    //Reset animation
+    console.log("a");
+}
